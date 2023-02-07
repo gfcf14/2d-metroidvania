c031749a7419a0e7681ecc26520aac42c834d28d
Carlos <carloscuba014@gmail.com>
2023-02-06 20:57:01 -0500

    ensure summoned enemies do not collide with non-summoned

--- 2D Metroidvania/Assets/Scripts/EnemyGrounder.cs
+++ 2D Metroidvania/Assets/Scripts/EnemyGrounder.cs
@@ -7,7 +7,7 @@ public class EnemyGrounder : MonoBehaviour {
   void Update(){}
 
   public void OnCollisionEnter2D(Collision2D col) {
-    if (Helpers.IsValueInArray(Constants.enemyNonColliderNames, col.gameObject.name)) {
+    if (Helpers.IsValueInArray(Constants.enemyNonColliderNames, col.gameObject.name.Replace("(Clone)", ""))) {
       Physics2D.IgnoreCollision(col.collider, GetComponent<BoxCollider2D>());
     }
   }
