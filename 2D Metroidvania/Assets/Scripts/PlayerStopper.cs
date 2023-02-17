5e2b12e3044fe13963b1379bf2a6a4b1ce06041b
Carlos <carloscuba014@gmail.com>
2023-02-16 21:16:25 -0500

    implement player stopper

---
+++ 2D Metroidvania/Assets/Scripts/PlayerStopper.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStopper : MonoBehaviour {
+  void Start() {
+    // GetComponent<SpriteRenderer>().color = new Color(0, 0, 0, 0);
+  }
+
+  void Update() {}
+
+  private void OnTriggerEnter2D(Collider2D col) {
+    if (col.gameObject.tag == "Hero") {
+      Hero hero = col.gameObject.GetComponent<Hero>();
+
+      if (hero.isAutonomous) {
+        hero.isAutonomous = false;
+        hero.isRunning = false;
+        col.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+      }
+    }
+  }
+}
