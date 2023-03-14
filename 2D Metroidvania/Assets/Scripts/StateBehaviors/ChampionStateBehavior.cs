8e7aa34e2e9f21afa0a794b6a61d602791843631
Carlos <carloscuba014@gmail.com>
2023-03-13 20:47:44 -0400

    ensure state behavior for skeleton thing is named champion

--- 2D Metroidvania/Assets/Scripts/StateBehaviors/ChampionStateBehavior.cs
+++ 2D Metroidvania/Assets/Scripts/StateBehaviors/ChampionStateBehavior.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class SkeletonKingStateBehavior : StateMachineBehaviour {
+public class ChampionStateBehavior : StateMachineBehaviour {
   public override void OnStateExit(Animator anim, AnimatorStateInfo stateInfo, int layerIndex) {
     if (stateInfo.IsName("defense")) {
       anim.ResetTrigger("isDefending");
