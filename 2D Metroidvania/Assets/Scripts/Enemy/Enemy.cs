4eba14551926c9d78750b5ddbfd57b53904bf17d
Carlos <carloscuba014@gmail.com>
2023-05-04 20:31:56 -0400

    avoid summon randomization when a key is present

--- 2D Metroidvania/Assets/Scripts/Enemy/Enemy.cs
+++ 2D Metroidvania/Assets/Scripts/Enemy/Enemy.cs
@@ -6,6 +6,7 @@ using System.Linq;
 public class Enemy : MonoBehaviour {
   // Serialized
     [SerializeField] public string key;
+    [SerializeField] public string summonKey;
     [SerializeField] public int level;
     [SerializeField] public bool isOnCamera = false;
 
@@ -652,7 +653,7 @@ public class Enemy : MonoBehaviour {
 
   public void Summon() {
     GameObject summonEnergy = Instantiate(Objects.prefabs["summon-energy"], new Vector3(transform.position.x + (isFacingLeft ? -1 : 1), transform.position.y, 0), Quaternion.identity);
-    summonEnergy.GetComponent<SummonEnergy>().summonKey = Constants.meadowEnemies[UnityEngine.Random.Range(0, Constants.meadowEnemies.Length)];
+    summonEnergy.GetComponent<SummonEnergy>().summonKey = summonKey != "" ? summonKey : Constants.meadowEnemies[UnityEngine.Random.Range(0, Constants.meadowEnemies.Length)];
     summonEnergy.GetComponent<SummonEnergy>().currentRoom = transform.parent.gameObject;
   }
 
