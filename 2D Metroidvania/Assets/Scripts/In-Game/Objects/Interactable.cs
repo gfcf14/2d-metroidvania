1c7513102bb7c6c3507549b35b46e8daad55bf76
Carlos <carloscuba014@gmail.com>
2023-08-22 20:40:55 -0400

    implement item rarity for interactables to give to droppables

--- 2D Metroidvania/Assets/Scripts/In-Game/Objects/Interactable.cs
+++ 2D Metroidvania/Assets/Scripts/In-Game/Objects/Interactable.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class Interactable : MonoBehaviour {
   [SerializeField] public bool isFacingLeft = true;
   [SerializeField] public string item;
+  [SerializeField] public string itemRarity;
   [System.NonSerialized] public bool isOpen = false;
   private Animator anim;
   private Chest chest;
@@ -44,8 +45,7 @@ public class Interactable : MonoBehaviour {
     Destroy(GetComponent<Rigidbody2D>());
     GetComponent<BoxCollider2D>().enabled = false;
 
-    // TODO: define what items will Interactables drop
-    // TODO: define a property that would allow to use different rarities
-    inGame.InstantiatePrefab("droppable", item, Helpers.IsValueInArray(Constants.moneyItemKeys, item) ? "money" : "normal", transform.parent.gameObject, transform, spriteRenderer);
+    string rarity = itemRarity != "" ? itemRarity : (Helpers.IsValueInArray(Constants.moneyItemKeys, item) ? "money" : "normal");
+    inGame.InstantiatePrefab("droppable", item, rarity, transform.parent.gameObject, transform, spriteRenderer);
   }
 }
