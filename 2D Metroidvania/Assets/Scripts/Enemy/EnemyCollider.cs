a21d8a00050b35f6b5c574bfef1c8207b46c5cc7
Carlos <carloscuba014@gmail.com>
2023-08-30 20:31:57 -0400

    ensure no more sword hits occur after enemy dies

--- 2D Metroidvania/Assets/Scripts/Enemy/EnemyCollider.cs
+++ 2D Metroidvania/Assets/Scripts/Enemy/EnemyCollider.cs
@@ -10,21 +10,25 @@ public class EnemyCollider : MonoBehaviour {
   void Update() {}
 
   private void OnCollisionEnter2D(Collision2D col) {
-    if (col.gameObject.name == "EnemyCollider") {
-      Physics2D.IgnoreCollision(col.gameObject.GetComponent<CapsuleCollider2D>(), GetComponent<CapsuleCollider2D>());
-    } else {
-      enemy.Collision(col);
+    if (!enemy.isDead) {
+      if (col.gameObject.name == "EnemyCollider") {
+        Physics2D.IgnoreCollision(col.gameObject.GetComponent<CapsuleCollider2D>(), GetComponent<CapsuleCollider2D>());
+      } else {
+        enemy.Collision(col);
+      }
     }
   }
 
   private void OnTriggerEnter2D(Collider2D col) {
-    if (col.gameObject.tag == "Weapon" && col.gameObject.name != "ThrowableCollider" && col.gameObject.name != "ArrowCollider") {
-      if (!col.gameObject.GetComponent<Weapon>().triggeredObjects.Contains(gameObject)) {
+    if (!enemy.isDead) {
+      if (col.gameObject.tag == "Weapon" && col.gameObject.name != "ThrowableCollider" && col.gameObject.name != "ArrowCollider") {
+        if (!col.gameObject.GetComponent<Weapon>().triggeredObjects.Contains(gameObject)) {
+          enemy.Trigger(col);
+          col.gameObject.GetComponent<Weapon>().triggeredObjects.Add(gameObject);
+        }
+      } else {
         enemy.Trigger(col);
-        col.gameObject.GetComponent<Weapon>().triggeredObjects.Add(gameObject);
       }
-    } else {
-      enemy.Trigger(col);
     }
   }
 }
