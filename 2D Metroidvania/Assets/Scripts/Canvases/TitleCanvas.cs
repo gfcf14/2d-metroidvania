1705b3ef5501eed97db01bbb0466948d21d0dbe9
Carlos <carloscuba014@gmail.com>
2023-11-11 20:29:39 -0500

    play select sound on title screen any key press

--- 2D Metroidvania/Assets/Scripts/Canvases/TitleCanvas.cs
+++ 2D Metroidvania/Assets/Scripts/Canvases/TitleCanvas.cs
@@ -7,11 +7,18 @@ public class TitleCanvas : MonoBehaviour {
   [SerializeField] GameObject pressPrompt;
   [SerializeField] GameObject buttonsFirstSelected;
   [SerializeField] EventSystem eventSystem;
+  [SerializeField] AudioSource audioSource;
 
-  void Start() {}
+  void Start() {
+    audioSource = GetComponent<AudioSource>();
+  }
 
   void Update() {
     if (Input.anyKey && !buttonsPanel.activeSelf) {
+      if (Settings.playSFX) {
+        audioSource.PlayOneShot(Sounds.menuSounds["select"]);
+      }
+
       pressPrompt.SetActive(false);
       buttonsPanel.SetActive(true);
       eventSystem.SetSelectedGameObject(buttonsFirstSelected, new BaseEventData(eventSystem));
