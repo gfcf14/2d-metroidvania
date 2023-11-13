ec7a358bb3d0e62bae6ea136e68746bbdea3b953
Carlos <carloscuba014@gmail.com>
2023-11-12 20:25:14 -0500

    ensure mouse doesn't trigger start in title screen

--- 2D Metroidvania/Assets/Scripts/Canvases/TitleCanvas.cs
+++ 2D Metroidvania/Assets/Scripts/Canvases/TitleCanvas.cs
@@ -14,14 +14,28 @@ public class TitleCanvas : MonoBehaviour {
   }
 
   void Update() {
-    if (Input.anyKey && !buttonsPanel.activeSelf) {
-      if (Settings.playSFX) {
-        audioSource.PlayOneShot(Sounds.menuSounds["select"]);
+    if (Input.anyKeyDown) {
+      string currentKey = "";
+      // Iterate through all possible keys
+      foreach (KeyCode keyCode in System.Enum.GetValues(typeof(KeyCode))) {
+        // Check if the current key or mouse button is pressed
+        if (Input.GetKey(keyCode)) {
+            // Gets the name of the pressed key or mouse button
+            currentKey = keyCode.ToString();
+            break;  // Exit the loop after finding the first pressed key
+        }
       }
 
-      pressPrompt.SetActive(false);
-      buttonsPanel.SetActive(true);
-      eventSystem.SetSelectedGameObject(buttonsFirstSelected, new BaseEventData(eventSystem));
+      // only proceed with showing the title menu if the key pressed is not a mouse key and if it isn't active
+      if (!currentKey.Contains("Mouse") && !buttonsPanel.activeSelf) {
+        if (Settings.playSFX) {
+          audioSource.PlayOneShot(Sounds.menuSounds["select"]);
+        }
+
+        pressPrompt.SetActive(false);
+        buttonsPanel.SetActive(true);
+        eventSystem.SetSelectedGameObject(buttonsFirstSelected, new BaseEventData(eventSystem));
+      }
     }
   }
 
