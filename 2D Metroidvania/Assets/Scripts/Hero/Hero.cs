f6183183026d5641a4767e251c58483da7e60152
Carlos <carloscuba014@gmail.com>
2024-03-25 11:33:08 -0400

    install URP and remap keyboard arrow input

--- 2D Metroidvania/Assets/Scripts/Hero/Hero.cs
+++ 2D Metroidvania/Assets/Scripts/Hero/Hero.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Hero : MonoBehaviour {
   [System.NonSerialized] public bool showDebug = false;
