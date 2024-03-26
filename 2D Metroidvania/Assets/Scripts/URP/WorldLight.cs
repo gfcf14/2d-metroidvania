56564cc7402ae7eec3f56f9e52f95fabf792b24a
Carlos <carloscuba014@gmail.com>
2024-03-26 14:35:14 -0400

    adjust day light change to game time

--- 2D Metroidvania/Assets/Scripts/URP/WorldLight.cs
+++ 2D Metroidvania/Assets/Scripts/URP/WorldLight.cs
@@ -7,30 +7,38 @@ namespace WorldTime {
   public class WorldLight : MonoBehaviour {
     [SerializeField] Gradient gradient;
 
-    // TODO: duration might not even be needed. Try to plug in Constants.maxDayLength in its place
-    public float duration;
+    // TODO: remove these test variables once no longer used
+    [SerializeField] string currentTime;
+    [SerializeField] float currentGradientPercentage;
+
+    [System.NonSerialized] public Hero hero;
     private Light2D light;
-    private float startTime;
+    // private float startTime;
 
     void Awake() {
+      hero = GameObject.FindGameObjectWithTag("Hero").GetComponent<Hero>();
       light = GetComponent<Light2D>();
       startTime = Time.time;
     }
 
     void Update() {
-      // TODO: perform only if not paused
+      if (!hero.isPaused) {
+        // gets elapsed time since start
+        // TODO: add time from loaded time once implemented
+        float elapsedTime = Time.time + GameData.initialGameTime;
+        // float elapsedTime = Time.time - startTime;
+        currentTime = Helpers.GetGameTime((int)(elapsedTime % Constants.maxDayLength));
 
-      // gets elapsed time since start
-      // TODO: add time from loaded time once implemented
-      float elapsedTime = Time.time - startTime;
+        // gets the percentage based on the sine of the time elapsed
+        float percentage = (elapsedTime % Constants.maxDayLength) / Constants.maxDayLength;
+        // float percentage = Mathf.Sin(elapsedTime / 30 * Mathf.PI * 2) * 0.5f + 0.5f;
 
-      // gets the percentage based on the sine of the time elapsed
-      float percentage = Mathf.Sin(elapsedTime / duration * Mathf.PI * 2) * 0.5f + 0.5f;
+        // clamps the percentage to be between 0 and 1
+        percentage = Mathf.Clamp01(percentage);
+        currentGradientPercentage = percentage;
 
-      // clamps the percentage to be between 0 and 1
-      percentage = Mathf.Clamp01(percentage);
-
-      light.color = gradient.Evaluate(percentage);
+        light.color = gradient.Evaluate(percentage);
+      }
     }
   }
 }
