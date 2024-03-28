4612894a0fd27eaf36c286524840137b7b8a9773
Carlos <carloscuba014@gmail.com>
2024-03-27 20:37:16 -0400

    refine global light gradient colors

--- 2D Metroidvania/Assets/Scripts/URP/WorldLight.cs
+++ 2D Metroidvania/Assets/Scripts/URP/WorldLight.cs
@@ -12,26 +12,22 @@ namespace WorldTime {
     [SerializeField] float currentGradientPercentage;
 
     [System.NonSerialized] public Hero hero;
-    private Light2D light;
-    // private float startTime;
+    private new Light2D light;
 
     void Awake() {
       hero = GameObject.FindGameObjectWithTag("Hero").GetComponent<Hero>();
       light = GetComponent<Light2D>();
-      startTime = Time.time;
     }
 
     void Update() {
       if (!hero.isPaused) {
-        // gets elapsed time since start
+        // gets elapsed time since start (to test hours, add values in multiples of 60 after GameData.initialGameTime)
         // TODO: add time from loaded time once implemented
         float elapsedTime = Time.time + GameData.initialGameTime;
-        // float elapsedTime = Time.time - startTime;
         currentTime = Helpers.GetGameTime((int)(elapsedTime % Constants.maxDayLength));
 
         // gets the percentage based on the sine of the time elapsed
         float percentage = (elapsedTime % Constants.maxDayLength) / Constants.maxDayLength;
-        // float percentage = Mathf.Sin(elapsedTime / 30 * Mathf.PI * 2) * 0.5f + 0.5f;
 
         // clamps the percentage to be between 0 and 1
         percentage = Mathf.Clamp01(percentage);
