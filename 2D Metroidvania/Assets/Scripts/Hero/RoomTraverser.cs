5023453bef6daa203a85fe4ff33aa0b625a812ba
Carlos <carloscuba014@gmail.com>
2024-05-24 20:32:44 -0400

    disable y displacement for room entry when dropkicking
    Since downward speed is huge, thus potentially removing the player from his "lane" and outside the game area

--- 2D Metroidvania/Assets/Scripts/Hero/RoomTraverser.cs
+++ 2D Metroidvania/Assets/Scripts/Hero/RoomTraverser.cs
@@ -12,7 +12,7 @@ public class RoomTraverser : MonoBehaviour {
     if (col.tag == "Room") {
       // if entering room, move based on speed to scape the previous room's bounds. Avoid displacements if on inclines
       float xDisp = ((hero.heroWidth / 4) * hero.body.velocity.x) * (hero.groundType == "level" ? 1 : 0);
-      float yDisp = ((hero.heroHeight / 4) * hero.body.velocity.y) * (hero.groundType == "level" ? 1 : 0);
+      float yDisp = hero.isDropKicking ? 0 : ((hero.heroHeight / 4) * hero.body.velocity.y) * (hero.groundType == "level" ? 1 : 0);
 
       hero.transform.position = new Vector2(hero.transform.position.x + xDisp, hero.transform.position.y + yDisp);
     }
